ee56f4593d974e7cf082e2a84a6fbc4da1332e27
Chackraz <rafael.vaz.palmeira@gmail.com>
2025-08-09 13:19:20 +0200

    Update geneticrocket.js

--- geneticrocket.js
+++ geneticrocket.js
@@ -1,4 +1,11 @@
 //geneticAlgo-RocketSim
+ let target = null;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const logDiv = document.getElementById('log');
+const rocketPos = document.getElementById('rocketPos');
+const targetPos = document.getElementById('targetPos');
+
 canvas.addEventListener('click', (e) => {
       const rect = canvas.getBoundingClientRect();
       target = { x: e.clientX - rect.left, y: e.clientY - rect.top };
